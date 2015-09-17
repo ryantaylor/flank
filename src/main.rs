ebb90d81148faa69e1d7bf9e86fc4ceeb22b64d3
ryantaylor <ryan.taylor9@gmail.com>
2015-09-17 10:08:55 -0400

    Added some rudimentary command line argument support. You can now specify the replay to parse.

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,41 @@
 extern crate log4rs;
 extern crate rustc_serialize;
 extern crate vault;
+extern crate getopts;
 
 use std::default::Default;
+use std::env;
 use std::path::Path;
 
 use rustc_serialize::json;
+use getopts::Options;
 
 use vault::replay::Replay;
 
 fn main() {
-    // Initialize logging
-    log4rs::init_file("log.toml", Default::default()).unwrap();
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optflag("l", "log", "enable logging to stdout");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(mtch) => mtch,
+        Err(err) => panic!(err.to_string()),
+    };
+
+    if matches.opt_present("l") {
+        log4rs::init_file("log.toml", Default::default()).unwrap();
+    }
+
+    let input = if !matches.free.is_empty() {
+        matches.free[0].clone()
+    } else {
+        return;
+    };
 
     // Create a path to the desired file
-    let path = Path::new("/home/ryan/replays/city_17_4v4.rec");
+    let path = Path::new(&input);
 
     let mut replay = Replay::new(&path);
     replay.parse();
